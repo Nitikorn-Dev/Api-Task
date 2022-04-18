c6a039d5bd115c2ef73c7d6d48c57587a48ad918
Nitikorn-Dev <78679311+Nitikorn-Dev@users.noreply.github.com>
2022-04-18 21:43:18 +0700

    new commit

--- src/utils/custom-error/custom-error.model.ts
+++ src/utils/custom-error/custom-error.model.ts
@@ -1,10 +1,10 @@
 
-export class CustomError extends Error { 
+export class CustomError extends Error {
     constructor(
         protected msg: string | Object | any,
         public status: number = 500,
         public description?: any
-    ) { 
+    ) {
         super();
         // Object.setPrototypeOf(this, new.target.prototype);
         // Error.captureStackTrace(this);
@@ -54,61 +54,3 @@ export class InternalServerErrorException extends CustomError {
         super(msg, 500, description)
     }
 }
-
-
-// export enum HttpStatusCode {
-//     OK = 200,
-//     BAD_REQUEST = 400,
-//     NOT_FOUND = 404,
-//     INTERNAL_SERVER = 500,
-// }
-
-// export class BaseError extends Error {
-//     constructor(
-//         public readonly name: string,
-//         public readonly httpCode: HttpStatusCode,
-//         public readonly description: string,
-//         public readonly isOperational: boolean,
-//     ) {
-//         super(description);
-//         Object.setPrototypeOf(this.description, new.target.prototype);
-//         Error.captureStackTrace(this);
-//     }
-// }
-
-// class APIError extends BaseError {
-//     constructor(
-//         name: string,
-//         httpCode = HttpStatusCode.INTERNAL_SERVER,
-//         description = 'internal server error',
-//         isOerational = true,
-//     ) {
-//         super(name, httpCode, description, isOerational)
-//     }
-// }
-// class HTTP400Error extends BaseError {
-//     constructor(description = 'bad request') {
-//         super('NOT FOUND', HttpStatusCode.BAD_REQUEST, description, true);
-//     }
-// }
-
-
-
-// class ErrorHandler {
-//     public async handleError(err: Error): Promise<void> {
-//       await logger.error(
-//         'Error message from the centralized error-handling component',
-//         err,
-//       );
-//     //   await sendMailToAdminIfCritical();
-//     //   await sendEventsToSentry();
-//     }
-    
-//     public isTrustedError(error: Error) {
-//       if (error instanceof BaseError) {
-//         return error.isOperational;
-//       }
-//       return false;
-//     }
-//    }
-//    export const errorHandler = new ErrorHandler();
